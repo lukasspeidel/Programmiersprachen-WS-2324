cb5da3e5aaeb4d12ae612750e64b9feae9081b60
lukasspeidel <lukas.speidel@hfg.design>
2023-11-30 02:06:17 +0100

    Jquery implimentierung

--- quartettspiel_neu/script/dinosaur_data.js
+++ quartettspiel_neu/script/dinosaur_data.js
@@ -1,4 +1,4 @@
-const data = [
+const dinosaurs = [
     {
         "name": "Brachiosaurus",
         "group": "Large Sauropods",
--- quartettspiel_neu/script/main.js
+++ quartettspiel_neu/script/main.js
@@ -1,110 +1,98 @@
-<!DOCTYPE html>
-<html lang="en">
+$(document).ready(function () {
+    createCards();
+});
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quartett Spiel</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css?family=Krona One"
-        rel="stylesheet">
+function createCards() {
+    const iconPath = "img/icons/";
 
-    <link rel="stylesheet" href="styles.css">
-</head>
+    dinosaurs.forEach(dino => {
+        let card = document.createElement("div");
+        card.className = "card-wrapper";
+        const imagePath = "img/" + dino.group_letter + dino.card_number + "_" + dino.name.replace(/\s/g, '') + ".jpg";
 
-<body>
-
-    <div id="cards_container">
+        card.innerHTML = `
         <div class="whole_card">
             <div class="banner">
                 <div class="group_number primary_color_red">
-                    <h1>XY</h1>
+                    <h1>${dino.group_letter}</h1>
                 </div>
                 <div class="name">
-                    <h1>Whatever Dino</h1>
+                    <h1>${dino.name}</h1>
                 </div>
             </div>
             <div class="picture">
-                <img src="img\b3_allosaurus.jpg" alt="b3_allosaurus">
+                <img src="${imagePath}" alt="Image of a ${dino.name}" onerror="this.src='img/dummydino.png'">
             </div>
             <div class="facts secondary_color_red">
-                <h3>Group Name</h3>random facts random facts random facts random facts
+                <h3>${dino.group}</h3>${dino.random_fact}
             </div>
             <div class="attributes weight">
                 <div class="attributes_description primary_color_red">
                     <div class="icon_container">
-                        <img src="icons/weight_icon.png" alt="Weight icon">
+                        <img src="${iconPath}weight_icon.png" alt="Weight icon">
                     </div>
                     <div class="actual_attribute">
                         <h3>Weight</h3>
                     </div>
                 </div>
-                <div class="weight_info secondary_color_red">info</div>
+                <div class="weight_info secondary_color_red">${dino.weight}</div>
             </div>
             <div class="attributes number_eggs">
                 <div class="attributes_description primary_color_red">
                     <div class="icon_container">
-                        <img src="icons/number_eggs_icon.png" alt="Egg icon">
+                        <img src="${iconPath}number_eggs_icon.png" alt="Egg icon">
                     </div>
                     <div class="actual_attribute">
                         <h3>Eggs</h3>
                     </div>
                 </div>
-                <div class="number_eggs_info secondary_color_red">info</div>
+                <div class="number_eggs_info secondary_color_red">${dino.number_eggs}</div>
             </div>
             <div class="attributes speed">
                 <div class="attributes_description primary_color_red">
                     <div class="icon_container">
-                        <img src="icons/speed_icon.png" alt="Speed icon">
+                        <img src="${iconPath}speed_icon.png" alt="Speed icon">
                     </div>
                     <div class="actual_attribute">
                         <h3>Speed</h3>
                     </div>
                 </div>
-                <div class="speed_info secondary_color_red">info</div>
+                <div class="speed_info secondary_color_red">${dino.speed}</div>
             </div>
             <div class="attributes iq">
                 <div class="attributes_description primary_color_red">
                     <div class="icon_container">
-                        <img src="icons/iq_icon.png" alt="IQ icon">
+                        <img src="${iconPath}iq_icon.png" alt="IQ icon">
                     </div>
                     <div class="actual_attribute">
                         <h3>IQ</h3>
                     </div>
                 </div>
-                <div class="iq_info secondary_color_red">info</div>
+                <div class="iq_info secondary_color_red">${dino.iq}</div>
             </div>
             <div class="attributes number_skeletons">
                 <div class="attributes_description primary_color_red">
                     <div class="icon_container">
-                        <img src="icons/number_skeletons_icon.png" alt="Skeleton icon">
+                        <img src="${iconPath}number_skeletons_icon.png" alt="Skeleton icon">
                     </div>
                     <div class="actual_attribute">
                         <h3>Skeleton</h3>
                     </div>
                 </div>
-                <div class="number_skeletons_info secondary_color_red">info</div>
+                <div class="number_skeletons_info secondary_color_red">${dino.number_skeletons}</div>
             </div>
             <div class="attributes jaw_size">
                 <div class="attributes_description primary_color_red">
                     <div class="icon_container">
-                        <img src="icons/jaw_size_icon.png" alt="Jaw icon">
+                        <img src="${iconPath}jaw_size_icon.png" alt="Jaw icon">
                     </div>
                     <div class="actual_attribute">
                         <h3>Jaw Size</h3>
                     </div>
                 </div>
-                <div class="jaw_size_info secondary_color_red">info</div>
-            </div>
-
-        </div>`;
-        
-
-    </div>
-    <script src="script/dinosaur_data.js" defer></script>
-    <script src="script/main.js" defer></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js" defer></script>
-</body>
-
-</html>
+                <div class="jaw_size_info secondary_color_red">${dino.jaw_size}</div>
+            </div>`;
+        document.getElementById("cards-container").appendChild(card);
+    }
+    );
+}
